ce5cb0db6fc4908d39c56b715b1bfb53ffa3108b
Chris Carr <krzysztof@Chriss-MacBook-Pro.local>
2019-11-18 18:15:29 -0500

    change to config namespace

--- env-config.js
+++ env-config.js
@@ -2,8 +2,8 @@ const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
   "process.env.BASE_URL": prod
-    ? "https://portfolio-chriskcarr.herokuapp.com"
+    ? "https://chriskcarr.herokuapp.com"
     : "http://localhost:3000",
-  "process.env.NAMESPACE": "https://portfolio-chriskcarr.herokuapp.com",
+  "process.env.NAMESPACE": "https://chriskcarr.herokuapp.com",
   "process.env.CLIENT_ID": "NDcCZJS6qYYUbe21DAQHRpiQHH36yMB6"
 };
